6db546ead7eb9236ef2ce8406754e6a1191aedea
user <user>
2021-04-27 23:53:53 +0300

    Update test/gnome_sort_test.py

--- test/gnome_sort_test.py
+++ test/gnome_sort_test.py
@@ -6,5 +6,4 @@ class GnomeSortTest(TestCase):
     def test_sort(self):
         array=[1,6,4,7,9,5,3,6,8,0,10,2]
         expected=[0,1,2,3,4,5,6,6,7,8,9,10]
-        print(expected)
         self.assertEqual(expected,sort(array))
