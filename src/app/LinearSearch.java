f6a8fad3ee963a53b83fa84058cf4bf2294922ab
user <user>
2021-08-04 22:12:43 +0300

    Add Linear Search

---
+++ src/app/LinearSearch.java
@@ -0,0 +1,14 @@
+package app;
+
+public class LinearSearch
+{
+    public int search(int[] array,int element)
+    {
+        for(int i=0;i<array.length;i++)
+        {
+            if(array[i]==element)
+                return i;
+        }
+        return -1;
+    }
+}
