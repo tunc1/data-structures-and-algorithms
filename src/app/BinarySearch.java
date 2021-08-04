9691d1c3b57371398b64f489456beb749414fb0c
user <user>
2021-08-04 22:49:13 +0300

    Add Binary Search

---
+++ src/app/BinarySearch.java
@@ -0,0 +1,24 @@
+package app;
+
+public class BinarySearch
+{
+    public int search(int[] array,int element)
+    {
+        return search(array, element, 0, array.length);
+    }
+    private int search(int[] array,int element,int start,int end)
+    {
+        if(end-start==1)
+        {
+            if(array[start]==element)
+                return start;
+            return -1;
+        }
+        int middleIndex=(start+end)/2;
+        if(array[middleIndex]>element)
+            return search(array, element,0,middleIndex);
+        else if(array[middleIndex]>element)
+            return search(array, element,middleIndex,end);
+        return middleIndex;
+    }
+}
